09092c2dc32bb4e9a663bf892c6e432a6a2c5c34
sean <spcavanaugh88@gmail.com>
2021-04-05 15:53:22 -0400

    spelling correction

--- Lab 2 Day 1 Room Measurement/Program.cs
+++ Lab 2 Day 1 Room Measurement/Program.cs
@@ -17,10 +17,10 @@ namespace Lab_2_Day_1_Room_Measurement
                 Console.WriteLine("Enter Length");
                 double length = double.Parse(Console.ReadLine());
                 double area = length * width;
-                double perimiter = (length * 2) + (width * 2);
+                double perimeter = (length * 2) + (width * 2);
 
                 //calculate supplies needed
-                double paint = perimiter / 5;
+                double paint = perimeter / 5;
                 double carpet = area / 5;
 
 
@@ -29,7 +29,7 @@ namespace Lab_2_Day_1_Room_Measurement
                 Console.WriteLine($"Width: {width}");
                 Console.WriteLine($"Length: {length}");
                 Console.WriteLine($"Area: {area}");
-                Console.WriteLine($"Perimiter: {perimiter}");
+                Console.WriteLine($"Perimiter: {perimeter}");
                 Console.WriteLine($"Paint: {paint}");
                 Console.WriteLine($"Carpet: {carpet}");
                 Console.WriteLine("Would you like to calculate another room y/n?");
